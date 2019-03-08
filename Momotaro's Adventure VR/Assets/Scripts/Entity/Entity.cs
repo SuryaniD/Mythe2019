de906d48f22c63b2c45b83f4f118842755aa1253
PeterSchreuder_Laptop <24441@ma-web.nl>
2019-03-08 14:11:41 +0100

    Begin - Entity basics
    Health for all beiings

---
+++ Momotaro's Adventure VR/Assets/Scripts/Entity/Entity.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Entity
+{
+    public class Entity : MonoBehaviour
+    {
+        enum teamTypes { Noone, Friendly, Enemy };
+
+        private float healthCurrent;
+        public float HealthCurrent
+        {
+            get { return healthCurrent; }
+            set { healthCurrent = value; }
+        }
+
+        /// <summary>
+        /// Sets the entity on non-active
+        /// </summary>
+        public void EntityDeactivate()
+        {
+            
+        }
+
+        /// <summary>
+        /// Revives the entity
+        /// </summary>
+        public void EntityRevive(Vector3 _position)
+        {
+
+        }
+
+    }
+}
+
+
