1d8fecddf6e3fd9c0541a2d5b6d4b72da4408343
LAPTOP\Jarno van Lierop <jarnovanlierop@gmail.com>
2019-03-18 09:44:18 +0100

    enemy attack push

---
+++ Momotaro's Adventure VR/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();        
+    }
+
+    void Update()
+    {
+     if (Input.GetKeyDown(KeyCode.Space))
+        {
+            attack();
+        }
+    }
+
+    void attack()
+    {
+        Debug.Log("attack animation");
+        anim.Play("Attack1h1");
+        takeDamage();
+    }
+    void takeDamage()
+    {
+        Debug.Log("damage taken");
+    }
+
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.//target name)
+    //    {
+    //        takeDamage();
+    //    }
+    //}
+
+  
+}
