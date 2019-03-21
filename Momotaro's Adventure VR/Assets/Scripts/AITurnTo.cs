bc7da73f918d78b24cab72a34d0442672509bcad
ItsStanMarsh <35196896+ItsStanMarsh@users.noreply.github.com>
2019-03-21 16:55:11 +0100

    small changes to the turning to

--- Momotaro's Adventure VR/Assets/Scripts/AITurnTo.cs
+++ Momotaro's Adventure VR/Assets/Scripts/AITurnTo.cs
@@ -9,12 +9,17 @@ public class AITurnTo : MonoBehaviour
     private void Update()
     {
         Vector3 direction = target.position - transform.position;
+
         float _y = direction.y;
+
         direction.y = direction.z;
+
         direction.z = -_y;
+
         Quaternion rotation = Quaternion.LookRotation(direction);
         rotation.z = rotation.y;
         rotation.y = 0;
+
         transform.localRotation = Quaternion.Lerp(transform.localRotation, rotation, speed * Time.deltaTime);
     }
 }
