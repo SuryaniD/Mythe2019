442432919df15e098bef46b9b4f6646f3aeddb32
PeterSchreuder_Laptop <24441@ma-web.nl>
2019-03-25 13:59:55 +0100

    Working - Non VR Controls
    Whats working:
    - Able to walk
    - Look
    - Attack (Cannot yet damage)

---
+++ Momotaro's Adventure VR/Assets/Scripts/Player/PlayerMoving.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoving : MonoBehaviour
+{
+
+    [SerializeField]
+    private float moveSpeed = 6f, gravity = 20.0f;
+
+    [Header("Gets the Main Camera")]
+    [SerializeField]
+    private Transform cameraTransform;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        //Find the current camera
+        cameraTransform = GameObject.Find("Main Camera").transform;
+    }
+
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+
+        if (controller.isGrounded)
+        {
+            MoveDirection();
+        }
+
+        FakeGravity();
+
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    void MoveDirection()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        moveDirection = cameraTransform.TransformDirection(moveDirection);
+        moveDirection *= moveSpeed;
+    }
+
+    void FakeGravity()
+    {
+        //Fake gravity
+        moveDirection.y -= gravity * Time.deltaTime;
+    }
+}
---
+++ Momotaro's Adventure VR/Assets/Scripts/Player/Weapon/WeaponAttack.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponAttack : MonoBehaviour
+{
+    private Animator anim;
+    private bool canAttack = true;
+    [SerializeField]
+    private float attackTime = 1f;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && canAttack)
+            Attack();
+    }
+
+
+    void Attack()
+    {
+        canAttack = false;
+        StartCoroutine(Alarm_canAttack(attackTime));
+
+        print("Attack");
+        anim.Play("KatanaAttackAnimation");
+
+
+    }
+
+    IEnumerator Alarm_canAttack(float _value)
+    {
+        yield return new WaitForSeconds(_value);
+
+        canAttack = true;
+    }
+}
