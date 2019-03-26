355cabbf6e00aaa5a3d76a199bead14293b5189b
ItsStanMarsh <35196896+ItsStanMarsh@users.noreply.github.com>
2019-03-26 11:41:02 +0100

    level script added(not finished)

---
+++ Momotaro's Adventure VR/Assets/Scripts/ColliderTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderTrigger : MonoBehaviour
+{
+    public string playerTag = "player";
+    public Vector3 resetPos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == playerTag)
+        {
+            other.transform.position = resetPos;
+        }
+    }
+}
---
+++ Momotaro's Adventure VR/Assets/Scripts/Imported/LevelLoopTrigger/LevelTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTrigger : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject.FindGameObjectWithTag("ChunkController").GetComponent<LevelController>().SpawnNextChunk();
+        Destroy(transform.parent.gameObject);
+    }
+}
