737b6365d9b579c07aa6f7eda5af571d10b67826
ItsStanMarsh <35196896+ItsStanMarsh@users.noreply.github.com>
2019-03-28 09:05:00 +0100

    Trigger
    trigger works better, changed a bit of the scrippies

--- Momotaro's Adventure VR/Assets/Scripts/LvlLoopTrigger/ColliderTrigger.cs
+++ Momotaro's Adventure VR/Assets/Scripts/LvlLoopTrigger/ColliderTrigger.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class ColliderTrigger : MonoBehaviour
 {
-    public string playerTag = "player";
-    public Vector3 resetPos;
+    public string playerTag = "Player";
+    public Transform resetPos;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == playerTag)
         {
-            other.transform.position = resetPos;
+            other.transform.position = resetPos.position;
         }
     }
 }
