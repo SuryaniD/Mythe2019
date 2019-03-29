970a272e2ff9f3aa387d28b088302bbcb75f0552
SuryaniD <bytebytermail@gmail.com>
2019-03-29 12:40:25 +0100

    death screen base created
    the things needed for it exist now, almost finished

---
+++ Momotaro's Adventure VR/Assets/Scripts/GameState/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerDies player; // this is just where you would put the script where the action for the playerdeath is
+     
+    private void Start()
+    {
+        player.Death += PlayerDeath;
+    }
+    public void PlayerDeath()
+    {
+        Debug.Log("player dies");
+        Time.timeScale = 0; // should probably be done with lerp 
+        // menu appears(menu exists currently)
+        // player has a choice between restart and quit
+        // quit closes application
+        // restart loads scene and resets variables
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quit");
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1;
+        Debug.Log("restart");    }
+}
---
+++ Momotaro's Adventure VR/Assets/Scripts/GameState/PlayerDies.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+
+public class PlayerDies : MonoBehaviour
+{
+    public Action Death;
+    void Start()
+    {
+        Debug.Log("gonna die");
+
+        if(Death != null)
+        Death();  
+    }
+
+   
+}
