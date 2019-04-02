ae729f455992d7f83ae42b91c78b7b9cad50afed
burhanos2 <24742@ma-web.nl>
2019-04-02 12:44:51 +0200

    navigation added, strafe mechanic WIP
    the enemy now walks up to the player and stops, then if the player moves away too much de repeats. started work on the strafe logic

---
+++ Momotaro's Adventure VR/Assets/Scripts/Imported/AI/EnemyNav.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+
+public class EnemyNav : MonoBehaviour
+{
+    [SerializeField]
+    private NavMeshAgent agent;
+    [SerializeField]
+    private Transform goal;
+
+    private float _xOffset = 1.1f;
+    private float _dist;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        CheckOffset();
+    }
+    
+    private void Update()
+    {
+        _dist = Vector3.Distance(agent.transform.position, goal.position);
+        CheckDist();
+    }
+
+    void CheckDist()
+    {
+        if (_dist < _xOffset)
+        {
+            agent.destination = agent.transform.position;
+            agent.isStopped = true;
+        }
+        else
+        {
+            CheckTarget();
+        }
+    }
+
+    void CheckOffset()
+    {
+        Renderer rend = goal.GetComponent<Renderer>();
+
+        if (rend != null)
+        { _xOffset = rend.bounds.size.x * 1.7f; }
+    }
+
+    void CheckTarget()
+    {
+        if (_dist > _xOffset)
+        {
+            agent.destination = goal.position;
+            agent.isStopped = false;
+        }
+    }
+
+
+}
